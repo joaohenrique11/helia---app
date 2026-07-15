d9ee81f588162a4ba443ca68d40895f6f1df0dd2
João Henrique <joaohenriquesilvacapito57@gmail.com>
2026-07-15 00:25:43 -0300

    feat: implement header layout with icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { BellRingingIcon, BookBookmarkIcon, ChatsTeardropIcon } from "phosphor-react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <StatusBar />
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <ChatsTeardropIcon size={30} color='#1AB65C' weight='duotone' />
+          <Text style={styles.headerLeftText}>Helia</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <BellRingingIcon size={30} color='#F4F4F4' weight='duotone' />
+          <BookBookmarkIcon size={30} color='#F4F4F4' weight='duotone' />
+        </View>
+      </View>
     </View>
   );
 }
@@ -11,7 +22,29 @@ export default function Index() {
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "#181A20",
     paddingHorizontal: 20,
-  }
+  },
+
+  header: {
+    marginTop: 60,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  headerRight: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  headerLeftText: {
+    color: "#F4F4F4",
+    fontSize: 24,
+    fontWeight: 800
+  },
 })
